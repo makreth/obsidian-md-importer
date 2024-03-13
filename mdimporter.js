99623d155e7d92267f15132789093128ece471b9
makreth <shepherdduncan06@gmail.com>
2024-03-12 22:34:51 -0700

    Updated html and scripts to fetch text data of uploaded md document

--- mdimporter.js
+++ mdimporter.js
@@ -1,6 +1,30 @@
 Hooks.on("renderSidebarTab", async (app, html) => {
     if(app instanceof JournalDirectory){
-        let button = $("<button class='import-md'><i class='fas fa-file-import'></i>Obsidian .md Import</button>")
+        let button = $("<button class='import-md'><i class='fas fa-file-import'></i>Obsidian .md Import</button>");
+        button.click(function(){
+            new MdImporter().render(true);
+        })
         html.find(".directory-footer").append(button);
     }
 });
+
+class MdImporter extends FormApplication{
+
+    static get defaultOptions(){
+        const options = super.defaultOptions;
+        options.id = "md-importer";
+        options.template = "modules/obsidian-md-importer/mdimporter.html";
+        options.classes.push("md-importer");
+        options.resizable = false;
+        options.height = "auto";
+        options.width = 400;
+        options.minimizable = true;
+        options.title = "Obsidian .md Importer";
+        return options;
+    }
+
+    async _updateObject(event, formData){
+        let fileText = await this.element.find("[name=file0]")[0].files[0].text()
+        console.log(fileText);
+    }
+}
