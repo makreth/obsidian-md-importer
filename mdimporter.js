3fb32702826087ade274a3dd038fa9e8186b9a6b
makreth <makreth.presuming870@8alias.com>
2024-03-23 16:04:36 -0700

    parent path extraction

--- mdimporter.js
+++ mdimporter.js
@@ -1,16 +1,16 @@
 Hooks.on("renderSidebarTab", async (app, html) => {
-    if(app instanceof JournalDirectory){
+    if (app instanceof JournalDirectory) {
         let button = $("<button class='import-md'><i class='fas fa-file-import'></i>Obsidian .md Import</button>");
-        button.click(function(){
+        button.click(function () {
             new MdImporter().render(true);
         })
         html.find(".directory-footer").append(button);
     }
 });
 
-class MdImporter extends FormApplication{
+class MdImporter extends FormApplication {
 
-    static get defaultOptions(){
+    static get defaultOptions() {
         const options = super.defaultOptions;
         options.id = "md-importer";
         options.template = "modules/obsidian-md-importer/mdimporter.html";
@@ -23,8 +23,43 @@ class MdImporter extends FormApplication{
         return options;
     }
 
-    async _updateObject(event, formData){
-        let fileText = await this.element.find("[name=file0]")[0].files[0].text()
-        console.log(fileText);
+    /*
+    * JS file schema
+    * name: string of name of file
+    * path: string of absolute path of file
+    * type: string of type, e.g. "text/markdown"
+    * webkitRelativePath: string of relative path, including filename at the end of the path.
+    * Relative from folder selected by user
+    * 
+    * Functions
+    * text(): grabs the text of a file if it contains text
+    * 
+    * All folder paths grabbed in Foundry are forward slash paths
+    */
+
+    async _updateObject(event, formData) {
+        let files = await this.element.find("[name=folder-select]")[0].files;
+        let objectUuidMapping = {};
+        Array.from(files).forEach((file) => {
+            let folderLevels = extractAllParentPaths(file);
+        });
+        Array.from(files).forEach((file) => {
+            console.log(file);
+        })
+    }
+
+    static extractAllParentPaths(path) {
+        if (path === null) {
+            return;
+        }
+        if (typeof path === "string" && path.length === 0) {
+            return;
+        }
+
+        current_path = "";
+        return path.split("/").map((path_node) => {
+            current_path += "/" + path_node;
+            return current_path;
+        }).reverse();
     }
 }
