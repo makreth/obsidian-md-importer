4a03d3f8a5304815fff6cd0ec2e51eb3b907e3eb
makreth <makreth.presuming870@8alias.com>
2024-03-25 00:10:25 -0700

    Implemented iterative object creation, though it has errors.

--- mdimporter.js
+++ mdimporter.js
@@ -38,28 +38,101 @@ class MdImporter extends FormApplication {
     */
 
     async _updateObject(event, formData) {
-        let files = await this.element.find("[name=folder-select]")[0].files;
-        let objectUuidMapping = {};
-        Array.from(files).forEach((file) => {
-            let folderLevels = extractAllParentPaths(file);
-        });
-        Array.from(files).forEach((file) => {
-            console.log(file);
-        })
+        let files = Array.from(await this.element.find("[name=folder-select]")[0].files);
+        let pathTextObjects = [];
+        for(const file in files){
+            let pathTextObject = {};
+            pathTextObject.path = file.webkitRelativePath;
+            pathTextObject.text = await file.text();
+            pathTextObjects.push(pathTextObject);
+        }
+        pathTextObjects.sort((a,b) => (b.path.match(/\//g) || []).length) - (a.path.match(/\//g) || []).length;
+
+        //TODO: Iterate on pathTextObjects to create documents robustly
+        
+        let pathObjectMapping = {};
+        for(const file of files.values()){
+            let parentPaths = MdImporter.extractAllParentPaths(file.webkitRelativePath);
+            if(parentPaths === null || parentPaths.length === 0){
+                console.error("Extracting parent paths failed");
+                return;
+            }
+            let entryDepth = parentPaths.length - 2;
+            let prevParent = null;
+            let i = 0;
+            for(const path of parentPaths){
+                if(path in pathObjectMapping){
+                    i++;
+                    continue;
+                }
+                if(i > 0){
+                    prevParent = pathObjectMapping[parentPaths[i - 1]];
+                }
+                if(i < entryDepth){
+                        pathObjectMapping[path] = await Folder.create({
+                            name : MdImporter.getFileNameFromPath(path),
+                            folder: prevParent,
+                            type: "JournalEntry"
+                        })
+                }
+                else if(entryDepth == i){
+                        pathObjectMapping[path] = await JournalEntry.create({
+                            name : MdImporter.getFileNameFromPath(path),
+                            folder: prevParent
+                        })
+                }
+                else{
+                    pathObjectMapping[path] = await JournalEntryPage.create({
+                        name: file.name.split(".")[0],
+                        text : {markdown: await file.text(),  format: 2}
+                    },
+                    {
+                        parent: prevParent
+                    })
+                }
+                i++;
+            }
+        }
+        console.log(pathObjectMapping);
+
+    }
+
+    static async createObjects(pathObjectMapping){
+        let paths = Object.keys(pathObjectMapping).toSorted(
+            (a,b) => (a.match(/\//g) || []).length - (b.match(/\//g) || []).length
+        );
+        let stringObjectMapping = {
+            "Folder" : Folder,
+            "JournalEntry" : JournalEntry,
+            "JournalEntryPage" : JournalEntryPage
+        }
+        for (const path of paths){
+            let objectToCreate = pathObjectMapping[path];
+            let classOfObject = stringObjectMapping[objectToCreate.documentName];
+            let createdObject = await classOfObject.create(objectToCreate.toObject());
+            console.log(createdObject);
+        }
     }
 
     static extractAllParentPaths(path) {
         if (path === null) {
             return;
         }
+        if (typeof path !== "string"){
+            return;
+        }
         if (typeof path === "string" && path.length === 0) {
             return;
         }
 
-        current_path = "";
+        let current_path = "";
         return path.split("/").map((path_node) => {
             current_path += "/" + path_node;
             return current_path;
-        }).reverse();
+        });
+    }
+
+    static getFileNameFromPath(path){
+        return path.split("/").reverse()[0];
     }
 }
